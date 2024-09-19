e45e9465c032520e35655c218d86113f14b27c20
danigp12 <danigp12@usal.es>
2024-09-19 20:40:41 +0200

    Mayor de tres

--- Semana 01/Mayor de Tres/src/App.java
+++ Semana 01/Mayor de Tres/src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   public static void main(String[] args)throws Exception{
+
+    Scanner scannerRef = new Scanner(System.in);
+    System.out.println( "Escriba un numero entero");
+    int primerNumero = scannerRef.nextInt();
+
+    System.out.println( "Escriba un segundo numero entero");
+    int segundoNumero = scannerRef.nextInt();
+
+    System.out.println( "Escriba un tercer numero entero");
+    int tercerNumero = scannerRef.nextInt();
+
+    int mayor = 0;
+
+        
+    if(primerNumero >= segundoNumero){
+        mayor = primerNumero;
+    }else{
+        mayor = segundoNumero;
     }
-}
+     if (mayor < tercerNumero) {
+        mayor = tercerNumero;
+     }
+        
+     System.out.printf( "El mayor numero es:  %d", mayor);
+     scannerRef.close();
+    }
+
+   }
