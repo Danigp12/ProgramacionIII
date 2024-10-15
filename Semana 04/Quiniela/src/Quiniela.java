44414c8e462e941c57373a2bc0908208236da6b2
danigp12 <danigp12@usal.es>
2024-10-15 20:08:24 +0200

    new file:   Semana 04/Quiniela/.vscode/settings.json 	new file:   Semana 04/Quiniela/README.md 	new file:   Semana 04/Quiniela/bin/Quiniela.class 	new file:   Semana 04/Quiniela/bin/partido/Partido.class 	new file:   Semana 04/Quiniela/src/Quiniela.java 	new file:   Semana 04/Quiniela/src/partido/Partido.java

---
+++ Semana 04/Quiniela/src/Quiniela.java
@@ -0,0 +1,54 @@
+import com.coti.tools.Esdia;
+
+import partido.Partido;
+
+//Ejercicio hecho en la versión 2 para que el tamaño de la tabla dependa del equipo de nombre más largo.
+public class Quiniela {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Introduzca los equipos que conforman los 15 partidos y los resultados:");
+        Partido[] partidos = new Partido[15];
+        int a = 0; //Será la longitud del nombre de equipo más largo.
+
+        //Bucle para recoger los datos de los partidos.
+        for (int i=0; i<partidos.length; i++) {
+           partidos[i] = new Partido();
+            
+            System.out.printf("\nDatos del partido %d: \n\n", i+1);
+
+            String promLocal = String.format("Equipo Local: ");
+            String local = Esdia.readString(promLocal);
+            partidos[i].setEquipoLocal(local);
+
+            String promVisitante = String.format("Equipo Visitante: ");
+            String visitante = Esdia.readString(promVisitante);
+            partidos[i].setEquipoVisitante(visitante);
+
+            String promResultado = String.format("Resultado (1,X,2): ");
+            String resultado = Esdia.readString(promResultado);
+            partidos[i].setResultado(resultado);
+
+            //Modifica el valor de a si el nombre de algun equipo es mas largo que todos los anteriores introducidos.
+            if (local.length()>a) {
+                a = local.length();
+            }
+            if (visitante.length()>a) {
+                a = visitante.length();
+            }
+            
+        }
+
+        //Imprimacion de la tabla.
+        System.out.printf("|" + "-".repeat(2*a + 18) + "|\n");
+        System.out.printf("|" + " ".repeat(a + 5) + "Quiniela" + " ".repeat(a + 5) + "|\n");
+        System.out.printf("|" + "-".repeat(2*a + 18) + "|\n");
+
+
+        for (int i=0; i<partidos.length; i++) {
+            System.out.printf("| %-"+ a + "s | %-" + a + "s " + " ".repeat(9) + "| %s |\n" , partidos[i].getEquipoLocal(),partidos[i].getEquipoVisitante(),partidos[i].getResultado());
+        }
+
+        System.out.printf("|" + "-".repeat(2*a + 18) + "|\n");
+
+}
+
+}
---
+++ Semana 04/Quiniela/src/partido/Partido.java
@@ -0,0 +1,33 @@
+package partido;
+
+public class Partido {
+    private String equipoLocal = "";
+    private String equipoVisitante = "";
+    private String resultado = "";
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+    public String getResultado() {
+        return resultado;
+    }
+    public void setResultado(String resultado) {
+        this.resultado = resultado;
+    }
+    
+
+
+
+
+
+
+}
