5750191bddf5279e2b5ace842aeeaae612d94b9a
danigp12 <danigp12@usal.es>
2024-10-24 19:46:13 +0200

    new file:   Semana 06/CochesCRUD/.vscode/settings.json 	new file:   Semana 06/CochesCRUD/README.md 	new file:   Semana 06/CochesCRUD/bin/Coches/Coches.class 	new file:   Semana 06/CochesCRUD/bin/CochesCRUDHashMap.class 	new file:   Semana 06/CochesCRUD/src/Coches/Coches.java 	new file:   Semana 06/CochesCRUD/src/CochesCRUDHashMap.java

---
+++ Semana 06/CochesCRUD/src/Coches/Coches.java
@@ -0,0 +1,46 @@
+﻿package Coches;
+
+public class Coches {
+    private String marca;
+    private String modelo;
+    private String color;
+    private boolean navegador = false;
+
+    public Coches(String marca, String modelo, String color, boolean navegador){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.navegador = navegador;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public void setNavegador(boolean navega){
+        if (navega) {
+            this.navegador = true;
+        }
+    }
+    public boolean getNavegador(){
+        return navegador;
+    }
+
+    public String getCocheComoString(){
+        return "Marca: " + marca + ", Modelo: " + modelo + ", Color: " + color + ", Navegador: "+ navegador;
+    }
+}
---
+++ Semana 06/CochesCRUD/src/CochesCRUDHashMap.java
@@ -0,0 +1,107 @@
+import java.util.HashMap;
+import static com.coti.tools.Esdia.*;
+import java.util.Map;
+import Coches.Coches;
+
+public class CochesCRUDHashMap {
+
+    private static HashMap<String, Coches> concesionario = new HashMap<>();
+    public static void main(String[] args) throws Exception {
+        int opcion;
+        do{
+            System.out.println("\n--- MENU CRUD ---");
+            System.out.println("1. Agregar coche");
+            System.out.println("2. Eliminar coche");
+            System.out.println("3. Modificar coche");
+            System.out.println("4. Listar coches");
+            System.out.println("5. Salir");
+            opcion = readInt("Ingrese una opción: ");
+
+            switch (opcion) {
+                case 1:
+                    agregarChoche();
+                    break;
+                case 2:
+                    eliminarCoche();
+                    break;
+                case 3:
+                    modificarCoche();
+                    break;
+                case 4:
+                    printarCoches();
+                    break;
+                case 5:
+                    System.out.println("Adiós");
+                    break;
+            
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        
+        }while (opcion != 5);
+    }
+
+    public static void agregarChoche(){
+        String matricula = readString("Escriba la matrícula: ");
+        String marca = readString("Escriba la marca: ");
+        String modelo = readString("Escriba el modelo: ");
+        String color = readString("Escriba el Color: ");
+        boolean navegador = yesOrNo("Escriba yes si tiene navegador o no si no lo tiene");
+
+            concesionario.put(matricula, new Coches(marca, modelo, color, navegador));
+
+    }
+
+    public static void eliminarCoche(){
+        String matricula = readString("Introduzca la matrícula del coche a eliminar:");
+
+        if (concesionario.containsKey(matricula)) {
+            concesionario.remove(matricula);
+            System.out.println("Coche eliminado.");
+        } else {
+            System.out.println("Matrícula no encontrada.");
+        }
+    }
+
+    public static void modificarCoche(){
+
+        String matricula = readString("Introduzca la matrícula del coche a modificar.");
+
+        if (concesionario.containsKey(matricula)) {
+            String nuevaMarca = readString("Escriba la nueva marca: ");
+            String nuevoModelo = readString("Escriba el nuevo modelo: ");
+            String nuevoColor = readString("Escriba el nuevo color: ");
+            boolean nuevoNavegador = yesOrNo("Indique si tiene naavegador o no (yes/no):") ;
+
+            Coches nuevoCoche = new Coches(nuevaMarca, nuevoModelo, nuevoColor, nuevoNavegador);
+
+            concesionario.put(matricula, nuevoCoche);
+
+        } else {
+            System.out.println("Matrícula no encontrada");
+        }
+    }
+
+    public static void printarCoches(){
+
+        if (concesionario.isEmpty()) {
+            System.out.println("El concesionario está vacío.");
+            return;
+        }
+
+        for (Map.Entry<String, Coches> entry : concesionario.entrySet()) {
+            String matricula = entry.getKey();
+            Coches coche = entry.getValue();
+
+            System.out.println(String.format("Matrícula: %s , %s\n", matricula, coche.getCocheComoString()));
+        }
+    }
+
+
+
+
+
+
+
+}
