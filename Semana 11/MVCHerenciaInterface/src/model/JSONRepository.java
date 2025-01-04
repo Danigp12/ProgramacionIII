1bc8b9aee2c40480f92897d8a84b9d62abc5c95a
danigp12 <danigp12@usal.es>
2025-01-04 16:14:01 +0100

    new file:   Semana 11/MVCHerenciaInterface/.vscode/settings.json 	new file:   Semana 11/MVCHerenciaInterface/README.md 	new file:   Semana 11/MVCHerenciaInterface/bin/controller/Controller.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/Alumno.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/JSONRepository$1.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/JSONRepository.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/MemoryRepository.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/Model.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/PersistenceRepository.class 	new file:   Semana 11/MVCHerenciaInterface/bin/model/TSVRepository.class 	new file:   Semana 11/MVCHerenciaInterface/bin/mvcherenciainterface/MVCHerenciaInterface.class 	new file:   Semana 11/MVCHerenciaInterface/bin/view/AppView.class 	new file:   Semana 11/MVCHerenciaInterface/bin/view/ConsolaListadoView.class 	new file:   Semana 11/MVCHerenciaInterface/bin/view/ConsolaSubMenusView.class 	new file:   Semana 11/MVCHerenciaInterface/bin/view/VoiceView.class 	new file:   Semana 11/MVCHerenciaInterface/lib/biblioteca11.jar 	new file:   Semana 11/MVCHerenciaInterface/lib/gson-2.10.1.jar 	new file:   Semana 11/MVCHerenciaInterface/lib/jadapter-for-native-tts-0.12.0.jar new file:   Semana 11/MVCHerenciaInterface/src/controller/Controller.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/Alumno.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/JSONRepository.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/MemoryRepository.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/Model.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/PersistenceRepository.java 	new file:   Semana 11/MVCHerenciaInterface/src/model/TSVRepository.java 	new file:   Semana 11/MVCHerenciaInterface/src/mvcherenciainterface/MVCHerenciaInterface.java 	new file:   Semana 11/MVCHerenciaInterface/src/view/AppView.java 	new file:   Semana 11/MVCHerenciaInterface/src/view/ConsolaListadoView.java 	new file:   Semana 11/MVCHerenciaInterface/src/view/ConsolaSubMenusView.java 	new file:   Semana 11/MVCHerenciaInterface/src/view/VoiceView.java 	modified:   Semana 11/MVCHerenciaInterface/bin/view/VoiceView.class

---
+++ Semana 11/MVCHerenciaInterface/src/model/JSONRepository.java
@@ -0,0 +1,62 @@
+﻿package model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+public class JSONRepository implements PersistenceRepository{
+
+    @Override
+     public ArrayList<Alumno> importarAlumnos(){
+       Path ruta = Paths.get(System.getProperty("user.home"), "Desktop", "alumnos.json");
+            final File file = new File(ruta.toString());
+            Gson gson = new Gson();
+            try (FileReader reader = new FileReader(file)) {
+        // Se usa TypeToken para que el return sea del tipo especificado
+        TypeToken<List<Alumno>> token = new TypeToken<>() {};
+        ArrayList<Alumno> alumnos = new ArrayList<>();
+        alumnos = gson.fromJson(reader, token.getType());
+        return new ArrayList<>(alumnos);
+    } catch (FileNotFoundException e) {
+        System.err.println("File not found: " + e.getMessage());
+    } catch (Exception e) {
+        System.err.println("Error importing tasks: " + e.getMessage());
+    }
+    return new ArrayList<>();
+
+    
+    }
+
+
+
+
+    @Override
+     public boolean exportarAlumnos(ArrayList<Alumno> alumnos) {
+    Gson gson = new Gson();
+    String json = gson.toJson(alumnos);   
+    Path ruta = Paths.get(System.getProperty("user.home"), "Desktop", "alumnos.json");
+    
+       
+    try (FileWriter writer = new FileWriter(ruta.toFile())) {
+        writer.write(json);
+        return true;
+    } catch (IOException e) {
+        System.out.println(e.toString());
+       return false;
+    }
+   
+    }
+
+
+
+}
---
+++ Semana 11/MVCHerenciaInterface/src/view/VoiceView.java
@@ -0,0 +1,112 @@
+﻿package view;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static com.coti.tools.Esdia.*;
+
+
+import io.github.jonelo.jAdapterForNativeTTS.engines.SpeechEngine;
+import io.github.jonelo.jAdapterForNativeTTS.engines.SpeechEngineNative;
+import io.github.jonelo.jAdapterForNativeTTS.engines.Voice;
+import io.github.jonelo.jAdapterForNativeTTS.engines.VoicePreferences;
+import io.github.jonelo.jAdapterForNativeTTS.engines.exceptions.SpeechEngineCreationException;
+
+public class VoiceView extends ConsolaListadoView{
+
+    @Override
+    public void mostrarMenuPrincipal() throws InterruptedException{
+        String text = "Voz importada con éxito.";
+        try {
+            SpeechEngine speechEngine = SpeechEngineNative.getInstance();
+            List<Voice> voices = speechEngine.getAvailableVoices();
+    
+            // We want to find a voice according our preferences
+            VoicePreferences voicePreferences = new VoicePreferences();
+            voicePreferences.setLanguage("es"); //  ISO-639-1
+            voicePreferences.setCountry("ES"); // ISO 3166-1 Alpha-2 code
+            voicePreferences.setGender(VoicePreferences.Gender.FEMALE);
+            Voice voice = speechEngine.findVoiceByPreferences(voicePreferences);
+    
+            // simple fallback just in case our preferences didn't match any voice
+            if (voice == null) {
+                System.out.printf("Warning: Voice has not been found by the voice preferences %s%n", voicePreferences);
+                voice = voices.get(0); // it is guaranteed that the speechEngine supports at least one voice
+                System.out.printf("Using \"%s\" instead.%n", voice);
+            }
+    
+            speechEngine.setVoice(voice.getName());
+            speechEngine.say(text);
+
+            
+        int opcion;
+        do {
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("\n--- MENU ALUMNOS LISTADO COMPLETO ---");
+            speechEngine.say("--- MENU ALUMNOS LISTADO COMPLETO ---");
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println("1. Agregar Alumno");
+            speechEngine.say("1. Agregar Alumno");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("2. Eliminar Alumno por DNI");
+            speechEngine.say("2. Eliminar Alumno por DNI");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("3. Importar alumnos");
+            speechEngine.say("3. Importar alumnos");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("4. Exportar alumnos");
+            speechEngine.say("4. Exportar alumnos");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("5. Listar alumnos");
+            speechEngine.say("5. Listar alumnos");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("6. Salir");
+            speechEngine.say("6. Salir");
+            TimeUnit.SECONDS.sleep(2);
+
+            speechEngine.say("Ingrese una opción: ");
+            opcion = readInt("Ingrese una opción: ");
+            
+
+            switch (opcion) {
+                case 1:
+                    agregarAlumno();
+                    break;
+                case 2:
+                    eliminarAlumnoPorDNI();
+                    break;
+                case 3:
+                    importarAlumnos();
+                    break;
+                case 4:
+                    exportarAlumnos();
+                    break;
+                case 5:
+                    listarAlumnos();
+                    break;
+                case 6:
+                    speechEngine.say("Saliendo...");
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    speechEngine.say("Opción no válida.");
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 6);
+    
+        } catch (SpeechEngineCreationException | IOException e) {
+            System.err.println(e.getMessage());
+        }   
+    }
+
+   
+}
