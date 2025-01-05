57558493c3276b5d2cbe6d2a54e764490a1265bd
danigp12 <danigp12@usal.es>
2025-01-05 19:17:34 +0100

    modified:   Semana 12/ExceptionManagement/bin/App.class 	modified:   Semana 12/ExceptionManagement/src/App.java

--- Semana 12/ExceptionManagement/src/App.java
+++ Semana 12/ExceptionManagement/src/App.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //Ejercicio 1. Usar las customExceptions.
+
         try {
             int a = 2;
             int b = 0;
@@ -19,6 +21,7 @@ public class App {
         int b = 0;
         double cociente = 0;
         try{
+            cociente = (double) a/b;
             System.out.println(cociente);
         } catch (MyCustomUncheckedException e) {
             System.out.println("Error aritmético: " + e.getMessage());
@@ -36,6 +39,10 @@ public class App {
             System.out.println("Error: " + e);
         }
         System.out.println(2+2);
+
+        //Ejercicio 2: Vamos a usar la clase Persona para crearla de manera errónea y lanzar un mensaje de error.
+
+
     
     }
 }
